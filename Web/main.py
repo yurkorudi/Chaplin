4805268a7bc564bf354702d2c2239cc3ea580675
PaulStefanyshyn <126797697+PaulStefanyshyn@users.noreply.github.com>
2025-01-04 01:23:28 +0200

    phone impruve

--- Web/main.py
+++ Web/main.py
@@ -283,7 +283,7 @@ def button_click():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='192.168.221.143')
+    app.run(debug=True, host='192.168.0.100')
 
 
 
